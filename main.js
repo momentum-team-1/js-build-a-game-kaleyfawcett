181e00ac535ae6f256fc18791543cfca41f4a963
kaleyfawcett <kaley.fawcett@gmail.com>
2020-05-11 23:05:50 -0400

    Got some notes down and random code

---
+++ main.js
@@ -0,0 +1,82 @@
+
+// class Game {
+//     constructor () {
+
+let canvas = document.getElementById ("canvas");
+let context = canvas.getContext("2d");
+
+
+
+context.fillStyle = 'black';
+context.fillRect (20, 20, 200, 150)
+
+
+let gameSize = { x: canvas.width, y: canvas.height}
+
+this.bodies = []
+
+//create bodies for player, enemy to shoot, enemy to avoid, bullets, and bonus
+
+//make the player appear and also move back and forth & not go off the screen
+
+//make the player shoot the bullets up 
+
+//make the enemies and bonus move down 
+
+//make the enemies disappear after you hit them
+
+//keep score 
+
+//how to know when game is over
+
+//start over
+
+//opening screen with directions 
+
+////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+// draw (context); { 
+//     context.fillStyle = black; 
+//     context.fillRect (this.position.x, this.position.y, this.width, this.height);
+
+// }
+
+    
+
+// let player = new Player (gameWidth, gameHeight);
+
+// player.draw (context);
+
+    
+
+// context.fillstyle = '#000';
+// context.fillRect(20, 20, 100, 100);
+
+// class Player {
+//     constructor (gameWidth , gameHeight) {
+        
+//         this.width = 150;
+//         this.height = 30;
+
+//         this.position = {
+//             x: gameWidth /2 - this.width /2,
+//             y: gameHeight - this.height -10, 
+//         };
+
+
+
+//     }
+
+// let player = new Player (gameWidth, gameHeight);
+
+
+
+    // }
+
+    // }}
+
