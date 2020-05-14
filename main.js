191a273b9f2950adf6a8c5e1aef2d3495fa8d00a
Amy Gori <amydgori@gmail.com>
2020-05-13 21:25:32 -0400

    Some edits to fix syntax errors and help with bullet

--- main.js
+++ main.js
@@ -1,90 +1,85 @@
-
 class Game {
-    constructor () {
-    let canvas = document.getElementById ("gameboard");
-    let context = canvas.getContext("2d");
-    let gameSize = { x: gameboard.width, y: gameboard.height};
-    this.player = new Player (gameSize);
-    this.bullet = new Bullet (gameSize);
+  constructor () {
+    const canvas = document.getElementById('gameboard')
+    let context = canvas.getContext('2d')
+    let gameSize = { x: gameboard.width, y: gameboard.height }
+    this.player = new Player(this, gameSize)
     this.bodies = []
-    
-    let animate = () => {
-        this.update()
-        this.drawPlayer(context, gameSize)
-        this.drawBullet (context,gameSize)
-        
-        requestAnimationFrame(animate) 
+
+    // Here you can add the Player and then enemies
+    this.bodies = this.bodies.concat(this.player)
+
+    const animate = () => {
+      this.update()
+
+      for (let body of this.bodies){
+          this.drawBody(body, context, gameSize)
+      }
+
+      requestAnimationFrame(animate)
     }
-    animate () 
 
-}
+    animate()
+  }
 
-drawPlayer (context, gameSize) {
-    context.clearRect (0, 0, gameSize.x, gameSize.y)
-    context.fillStyle = "#000000"
-    let startingXPosition = this.player.center.x - this.player.size.x / 2
-    let startingYPosition = this.player.center.y - this.player.size.y / 2
-    let playerWidth = this.player.size.x
-    let playerHeight = this.player.size.y
-    context.fillRect(startingXPosition, startingYPosition, playerWidth, playerHeight)
-}
+  // Your drawPlayer and drawBullet methods are doing pretty much the same thing!
+  // So we can consolidate with a single method
+  // we'll pass in the body as an argument so the method knows which body it's dealing with
+  drawBody (body, context, gameSize) {
+    context.clearRect(0, 0, gameSize.x, gameSize.y)
+    context.fillStyle = '#000000'
+    const xPosition = body.center.x - body.size.x / 2
+    const yPosition = body.center.y - body.size.y / 2
+    const width = body.size.x
+    const height = body.size.y
+    context.fillRect(xPosition, yPosition, width, height)
+  }
 
-drawBullet (context, gameSize) {
-    // context.clearRect (0, 0, gameSize.x, gameSize.y)
-    context.fillStyle = "#000000"
-    let startingXPosition = this.player.center.x - this.player.size.x / 2
-    let startingYPosition = this.player.center.y - 20
-    let bulletWidth = this.bullet.size.x
-    let bulletHeight = this.bullet.size.y
-    context.fillRect(startingXPosition, startingYPosition, bulletWidth, bulletHeight)
-
-}
-update () {
-    this.player.update ()
-    
-}
+  update () {
+    // loop over bodies array to call the update method on each of them
+    for (let body of this.bodies) {
+        body.update()
+    }
+  }
 }
 
 class Player {
-    constructor (gameSize) {
-    this.size = {x: 30, y: 30}
-    this.center = {x: gameSize.x / 2, y: gameSize.y - this.size.y * 2 }
-    this.keyboarder = Keyboarder 
-}
+  constructor (game, gameSize) {
+    this.size = { x: 30, y: 30 }
+    this.center = { x: gameSize.x / 2, y: gameSize.y - this.size.y * 2 }
+    this.keyboarder = Keyboarder
+    this.game = game
+  }
 
-
-
-
-
-update () {
-    if (this.keyboarder.isDown (this.keyboarder.KEYS.LEFT)) {
-        this.center.x -= 2
-    } else if (this.keyboarder.isDown (this.keyboarder.KEYS.RIGHT)) {
-        this.center.x += 2
+  update () {
+    if (this.keyboarder.isDown(this.keyboarder.KEYS.LEFT)) {
+      this.center.x -= 2
+    } else if (this.keyboarder.isDown(this.keyboarder.KEYS.RIGHT)) {
+      this.center.x += 2
     }
     if (this.keyboarder.isDown(this.keyboarder.KEYS.S)) {
-        console.log ("pew pew")
-        let bullet = new Bullet ({ x: this.center.x, y: this.center.y - this.size.y - 10},
-            {x: 0, y: -7})    
-            this.keyboarder.KEYS.S = bullet 
-        }
-} 
+      console.log('pew pew')
+      const bullet = new Bullet(
+          { x: this.center.x, y: this.center.y - this.size.y - 10 },
+          { x: 0, y: -7 }
+        )
+    // add the bullet to the bodies array so you can call draw on it in the Game object
+      this.game.bodies.push(bullet)
+    }
+  }
 }
 
 class Bullet {
-    constructor (center, velocity) {
-        this.center = center
-        this.size = {x: 3, y:3}
-        this.velocity = velocity
-        
-    }
-    update () {
+  constructor (center, velocity) {
+    this.center = center
+    this.size = { x: 3, y: 3 }
+    this.velocity = velocity
+  }
 
-        this.center.x += this.velocity.x
-        this.center.y += this.velocity.y 
-       
-       
-    }
+  update () {
+    this.center.x += this.velocity.x
+    this.center.y += this.velocity.y
+  }
 }
 
 // class Enemies {
@@ -99,40 +94,30 @@ class Bullet {
 //     }
 // }
 
+new Game()
 
+/// //////////////////////////////////////////////////////////////////////////////////////////
 
+// create bodies for player, enemy to shoot, enemy to avoid, bullets, and bonus
 
-new Game ()
+// make the player appear and also move back and forth (DONE) & not go off the screen
 
+// make the player shoot the bullets up
 
+// make the enemies and bonus move down
 
+// make the enemies disappear after you hit them
 
+// make enemies and bonus not run into each other
 
-/////////////////////////////////////////////////////////////////////////////////////////////
+// keep score
 
-//create bodies for player, enemy to shoot, enemy to avoid, bullets, and bonus
+// how to know when game is over
 
-//make the player appear and also move back and forth (DONE) & not go off the screen
+// start over
 
-//make the player shoot the bullets up 
+// opening screen with directions
 
-//make the enemies and bonus move down 
-
-
-//make the enemies disappear after you hit them
-
-//make enemies and bonus not run into each other 
-
-//keep score 
-
-//how to know when game is over
-
-//start over
-
-//opening screen with directions 
-
-/////////////////////////////////////////////////////////////////////////////////////////////
-
+/// //////////////////////////////////////////////////////////////////////////////////////////
 
 // else if (this.center.x < 0) this.center.x = 0;
-
